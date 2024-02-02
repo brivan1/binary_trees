2bb24cadc96a0ae66af5880fb1905e6e71b926aa
Eunice-Serani <eaamable91@gmail.com>
2024-02-01 16:37:25 -0800

    Advance tasks

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,155 +1,39 @@
 #include "binary_trees.h"
-
-levelorder_queue_t *create_node(binary_tree_t *node);
-void free_queue(levelorder_queue_t *head);
-void push(binary_tree_t *node, levelorder_queue_t *head,
-		levelorder_queue_t **tail);
-void pop(levelorder_queue_t **head);
-int binary_tree_is_complete(const binary_tree_t *tree);
-
 /**
- * create_node - Creates a new levelorder_queue_t node.
- * @node: The binary tree node for the new node to contain.
- *
- * Return: If an error occurs, NULL.
- *         Otherwise, a pointer to the new node.
+ * bst_insert - inserts a value in a Binary Search Tree.
+ * @tree: double pointer to the root node of the BST to insert the value.
+ * @value: value to store in the node to be inserted.
+ * Return: Always 0 (Success)
  */
-levelorder_queue_t *create_node(binary_tree_t *node)
+bst_t *bst_insert(bst_t **tree, int value)
 {
-<<<<<<< HEAD
-	levelorder_queue_t *new;
-
-	new = malloc(sizeof(levelorder_queue_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->node = node;
-	new->next = NULL;
-
-	return (new);
-=======
-    if (tree == NULL)
-        return (0);
-
-    return binary_tree_is_complete_recursive(tree, 0, binary_tree_size(tree));
->>>>>>> 4b3aa0092651f133d4857e314c24b0a7bc7e9189
-}
-
-/**
- * free_queue - Frees a levelorder_queue_t queue.
- * @head: A pointer to the head of the queue.
- */
-void free_queue(levelorder_queue_t *head)
-{
-	levelorder_queue_t *tmp;
-
-	while (head != NULL)
-	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
-	}
-}
-
-/**
- * push - Pushes a node to the back of a levelorder_queue_t queue.
- * @node: The binary tree node to print and push.
- * @head: A double pointer to the head of the queue.
- * @tail: A double pointer to the tail of the queue.
- *
- * Description: Upon malloc failure, exits with a status code of 1.
- */
-void push(binary_tree_t *node, levelorder_queue_t *head,
-		levelorder_queue_t **tail)
-{
-<<<<<<< HEAD
-	levelorder_queue_t *new;
-
-	new = create_node(node);
-	if (new == NULL)
-	{
-		free_queue(head);
-		exit(1);
-	}
-	(*tail)->next = new;
-	*tail = new;
-}
-
-/**
- * pop - Pops the head of a levelorder_queue_t queue.
- * @head: A double pointer to the head of the queue.
- */
-void pop(levelorder_queue_t **head)
-{
-	levelorder_queue_t *tmp;
-
-	tmp = (*head)->next;
-	free(*head);
-	*head = tmp;
-=======
-    if (tree == NULL)
-        return (1);
-    if (index >= size)
-        return (0);
-
-    return (binary_tree_is_complete_recursive(tree->left, 2 * index + 1, size) &&
-            binary_tree_is_complete_recursive(tree->right, 2 * index + 2, size));
->>>>>>> 4b3aa0092651f133d4857e314c24b0a7bc7e9189
-}
-
-/**
- * binary_tree_is_complete - Checks if a binary tree is complete.
- * @tree: A pointer to the root node of the tree to traverse.
- *
- * Return: If the tree is NULL or not complete, 0.
- *         Otherwise, 1.
- *
- * Description: Upon malloc failure, exits with a status code of 1.
- */
-int binary_tree_is_complete(const binary_tree_t *tree)
-{
-<<<<<<< HEAD
-	levelorder_queue_t *head, *tail;
-	unsigned char flag = 0;
+	bst_t *aux = NULL;
 
 	if (tree == NULL)
-		return (0);
-
-	head = tail = create_node((binary_tree_t *)tree);
-	if (head == NULL)
-		exit(1);
-
-	while (head != NULL)
+		return (NULL);
+	if (*tree == NULL)
 	{
-		if (head->node->left != NULL)
-		{
-			if (flag == 1)
-			{
-				free_queue(head);
-				return (0);
-			}
-			push(head->node->left, head, &tail);
-		}
-		else
-			flag = 1;
-		if (head->node->right != NULL)
-		{
-			if (flag == 1)
-			{
-				free_queue(head);
-				return (0);
-			}
-			push(head->node->right, head, &tail);
-		}
-		else
-			flag = 1;
-		pop(&head);
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
 	}
-	return (1);
-=======
-    if (tree == NULL)
-        return (0);
-
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
->>>>>>> 4b3aa0092651f133d4857e314c24b0a7bc7e9189
+	aux = *tree;
+	if (value < aux->n)
+	{
+		if (aux->left == NULL)
+		{
+			aux->left = binary_tree_node(aux, value);
+			return (aux->left);
+		}
+		return (bst_insert(&(aux->left), value));
+	}
+	if (value > aux->n)
+	{
+		if (aux->right == NULL)
+		{
+			aux->right = binary_tree_node(aux, value);
+			return (aux->right);
+		}
+		return (bst_insert(&(aux->right), value));
+	}
+	return (NULL);
 }
